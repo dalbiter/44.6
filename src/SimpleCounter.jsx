ddea1fff9640e13f12742df362fb8911f18e5698
danx <rushn24@gmail.com>
2024-07-09 22:19:48 -0400

    setup vitest, jsdom, and React Testing Library

--- src/SimpleCounter.jsx
+++ src/SimpleCounter.jsx
@@ -3,13 +3,18 @@ import React, { useState } from 'react';
 const SimpleCounter = () => {
     const [ num, setNum ] = useState(0)
     const clickUp = () => {
-        setNum(num+1)
+        setNum(n => n + 1)
+    }
+    const clickUpBy2 = () => {
+        setNum(n => n + 1)
+        setNum(n => n + 1)
     }
 
     return (
         <div>
             <h3>Count: {num}</h3>
             <button onClick={clickUp}>Up</button>
+            <button onClick={clickUpBy2}>Up By 2</button>
         </div>
     )
 };
